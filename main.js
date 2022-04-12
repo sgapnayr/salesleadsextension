ead033643145879b0da7991675761b107978c01c
“Ryan” <pagsmanagement@gmail.com>
2022-04-12 13:22:47 -0600

    a tag

--- main.js
+++ main.js
@@ -1,15 +1,25 @@
-const { links } = require("express/lib/response");
 
-let myLeads = ["asdfa sd", "asdfs adf dsa"];
+let myLeads = [];
 const inputBtn = document.getElementById('input-btn');
 const inputEl = document.getElementById('input-el');
 const ulEl = document.getElementById('ul-el');
 
+
 inputBtn.addEventListener('click', () => {
-    for (let i = 0; i < myLeads.length; i++) {
-        const li = document.createElement("li");
-        li.textContent = myLeads[i];
-        ulEl.append(li);
-    }
+        myLeads.push(inputEl.value);
+        inputEl.value= "";
+        renderLeads();
+
 });
 
+function renderLeads() {
+    let listItems = "";
+    for (let i = 0; i < myLeads.length; i++) {
+        // listItems += "<li><a href='" + myLeads[i] + "'target=_blank'>" + myLeads[i] + "</a></li>"
+        listItems += 
+                    `<li>
+                        <a href='${myLeads[i]}' target='_blank'>${myLeads[i]}</a>
+                    </li>`
+    }
+    ulEl.innerHTML = listItems;
+}
